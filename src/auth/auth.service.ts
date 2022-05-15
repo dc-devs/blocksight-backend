6f6cb3d8b1b51328d38f268a8d36bee18f4d4d54
David Christian <david.w.christian@gmail.com>
2022-05-15 14:28:55 -0700

    Added User data to JWTT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -26,9 +26,7 @@ export class AuthService {
 	}
 
 	async login(user: User) {
-		const payload = { email: user.email, sub: user.id };
-
-		const access_token = this.jwtService.sign(payload);
+		const access_token = this.jwtService.sign({ data: user });
 
 		return {
 			user,
